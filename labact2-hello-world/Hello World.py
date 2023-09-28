2c259843c6ae6d58deb7ff891b7b7243879ad910
jocarbonell <145944210+jocarbonell@users.noreply.github.com>
2023-09-29 07:57:47 +0800

    Add files via upload

--- labact2-hello-world/Hello World.py
+++ labact2-hello-world/Hello World.py
@@ -1,3 +1,5 @@
+### Variables and Input
+
 name = input("What is your name?")
 age = input("What is your age?")
 age = int(age)
@@ -5,8 +7,13 @@ age = int(age)
 pi = 3.1415
 modulo = 5 % 3
 
+### Variables and Output
+
 print(f"Hello, {name}")
-print("Hello,",name, sep="%%%%", end="!")
+print("Hello,", name)
+print("Hello," + name)
+print("Hello,", name, sep="%%%%", end="!")
 print(f"My age next year is + {age + 1}")
 print(f"Pi value is {pi}")
 print(f"Modulo is {modulo}")
+print(f"Modulo is {5%3}")
