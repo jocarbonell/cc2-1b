4a0daee19c45430ec8833ba56c774cd31f8b1373
jocarbonell <145944210+jocarbonell@users.noreply.github.com>
2023-09-29 07:39:22 +0800

    Add files via upload

---
+++ labact2-hello-world/Hello World.py
@@ -0,0 +1,12 @@
+name = input("What is your name?")
+age = input("What is your age?")
+age = int(age)
+
+pi = 3.1415
+modulo = 5 % 3
+
+print(f"Hello, {name}")
+print("Hello,",name, sep="%%%%", end="!")
+print(f"My age next year is + {age + 1}")
+print(f"Pi value is {pi}")
+print(f"Modulo is {modulo}")
