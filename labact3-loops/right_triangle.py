1d2f7d405022461c79c7aa836cf377abc296bab5
jocarbonell <145944210+jocarbonell@users.noreply.github.com>
2023-10-13 23:47:15 +0800

    Add files via upload

--- labact3-loops/right_triangle.py
+++ labact3-loops/right_triangle.py
@@ -1,1 +1,17 @@
-df
+# Programmed by Jennylyn Carbonell
+
+# Right Triangle 
+
+# Start of the program
+# Variables
+rows = 1
+rows_final = 5 
+
+# Functions
+while rows <= rows_final: 
+  for i in range(1, rows+1): 
+    print(i, end=" ") 
+  print()
+  print()
+  rows += 1
+# End of the program
