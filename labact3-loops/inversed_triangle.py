f2e057ec293cc34ef033f42bd811d7597ebc81dd
jocarbonell <145944210+jocarbonell@users.noreply.github.com>
2023-10-13 23:49:18 +0800

    Add files via upload

---
+++ labact3-loops/inversed_triangle.py
@@ -0,0 +1,16 @@
+# Programmed by Jennylyn Carbonell
+
+# Inverse Right Triangle
+
+# Start of the program
+# Variables
+rows = 5
+rows_final = 1
+
+# Functions
+while rows >= rows_final: 
+  for i in range(1, rows+1): 
+    print(i, end=" ") 
+  print()
+  rows -= 1
+  # End of the program
